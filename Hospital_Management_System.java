e348a6fc25495b4bf7fb1138f1759356db052d78
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-02-18 00:12:03 +0500

    FIXI

--- Hospital_Management_System.java
+++ Hospital_Management_System.java
@@ -45,3 +45,58 @@ class hospital {
     HashMap<Integer, Integer> patient_age = new HashMap<>();
     HashMap<Integer, String> patient_disease = new HashMap<>();
     HashMap<Integer, String> patient_emergency_status = new HashMap<>();
+
+        void add_patient() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Do you want to add patient? (yes/no): ");
+        String add = sc.nextLine();
+
+        if (add.equals("yes")) {
+
+            System.out.println("Enter patient id: ");
+            int id = sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("Enter patient name: ");
+            String name = sc.nextLine();
+            patient_names.put(id, name);
+
+            System.out.println("Enter patient age: ");
+            int age = sc.nextInt();
+            patient_age.put(id, age);
+            sc.nextLine();
+
+            System.out.println("Enter patient disease: ");
+            String disease = sc.nextLine();
+            patient_disease.put(id, disease);
+
+            System.out.println("Emergency? (yes/no): ");
+            String emg = sc.nextLine();
+            patient_emergency_status.put(id, emg);
+
+            System.out.println(patient_names);
+            System.out.println(patient_age);
+            System.out.println(patient_disease);
+            System.out.println(patient_emergency_status);
+        }
+    }
+    void display_info() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter patient ID: ");
+        int id = sc.nextInt();
+
+        if (patient_names.containsKey(id) == true) {
+
+            System.out.println(
+                    "Patient Info: \nName: " + patient_names.get(id)
+                            + "\nAge: " + patient_age.get(id)
+                            + "\nDisease: " + patient_disease.get(id)
+                            + "\nEmergency Status: " + patient_emergency_status.get(id));
+        }
+        
+        else {
+            System.out.println("Invalid ID");
+        }
+    }
