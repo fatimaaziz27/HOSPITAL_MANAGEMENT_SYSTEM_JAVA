d9add0406714a9c8871b59729bec1eb4684570ea
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-02-18 00:09:52 +0500

    FIXI

---
+++ Hospital_Management_System.java
@@ -0,0 +1,8 @@
+// Question:
+// Write a Java program to create a Hospital Management System.
+// The program should:
+// Add patient details (ID, name, age, disease, emergency status).
+// Display patient information using patient ID.
+// Change a patient’s emergency status.
+// Show a menu and repeat until the user chooses to exit.
+
